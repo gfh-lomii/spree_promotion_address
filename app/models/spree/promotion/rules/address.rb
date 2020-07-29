0fd7afc5c2bdc106111cede9133fe56f52904e0e
chinoxchen <chienfu.udp@gmail.com>
2020-07-28 20:13:54 -0400

    first commit

---
+++ app/models/spree/promotion/rules/address.rb
@@ -0,0 +1,33 @@
+# A rule to apply to an order greater than (or greater than or equal to)
+# a specific amount
+module Spree
+  class Promotion
+    module Rules
+      class Address < PromotionRule
+        preference :address, :string, default: ''
+
+        def applicable?(promotable)
+          promotable.is_a?(Spree::Order)
+        end
+
+        def eligible?(order, _options = {})
+
+          ship_address = order&.ship_address&.address1&.downcase
+
+          valid_address = true
+          preferred_address.split(" ").each do |element|
+            unless ship_address.include? element.downcase
+              valid_address = false
+              break
+            end
+          end
+
+          eligibility_errors.add(:base, 'no tiene dirección configurada') unless ship_address.present?
+          eligibility_errors.add(:base, 'su dirección no tiene promoción') unless valid_address
+
+          eligibility_errors.empty?
+        end
+      end
+    end
+  end
+end
---
+++ lib/spree_promotion_address.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_promotion_address/engine'
+require 'spree_promotion_address/version'
